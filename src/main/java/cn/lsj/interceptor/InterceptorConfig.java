3e78a7358d8a45a78f93bff04591bcbec565f1d8
lushunjian <771243321@qq.com>
2018-08-27 10:37:40 +0800

    登录拦截

---
+++ src/main/java/cn/lsj/interceptor/InterceptorConfig.java
@@ -0,0 +1,43 @@
+package cn.lsj.interceptor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class InterceptorConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    public InterceptorConfig(){
+        super();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");
+        registry.addResourceHandler("/templates/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/templates/");
+        super.addResourceHandlers(registry);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //拦截规则：除了login，其他都拦截判断
+        registry.addInterceptor(new WebInterceptor()).addPathPatterns("/**").excludePathPatterns("/login");
+        super.addInterceptors(registry);
+    }
+
+}
