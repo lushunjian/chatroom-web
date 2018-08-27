cd3a06d1415139e9b33067467a404e140dbd986a
lushunjian <771243321@qq.com>
2018-08-27 17:05:58 +0800

    注释

--- src/main/java/cn/lsj/interceptor/InterceptorConfig.java
+++ src/main/java/cn/lsj/interceptor/InterceptorConfig.java
@@ -1,8 +1,5 @@
 package cn.lsj.interceptor;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
@@ -16,16 +13,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan
 public class InterceptorConfig extends WebMvcConfigurerAdapter{
 
-/**
+    /**
      * 静态文件映射
      * addResourceHandler指的是对外暴露的访问路径，addResourceLocations指的是文件放置的目录
      * 映射之后，Controller返回到 html ，使用映射后的路径
-     * 如下将 static 下的静态文件映射到根路径下，在 html 中就可以通过例如 semantic/semantic.min.js 的路径访问静态文件
+     * 如下将 static 下的静态文件映射到根路径下，
+     * 文件实际路径 /static/semantic/semantic.min.js 映射之后
+     * 在 html 中就可以通过 semantic/semantic.min.js 路径访问静态文件
+     *
+     * 也可映射绝对路径,例如下面注释，把D盘static中的静态文件，映射到项目根路径下
      * */
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-       // registry.addResourceHandler("/my/**").addResourceLocations("file:E:/my/");
+       // registry.addResourceHandler("/**").addResourceLocations("D:/static/");
         registry.addResourceHandler("/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");
         registry.addResourceHandler("/templates/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/templates/");
         super.addResourceHandlers(registry);
